b568aba584e79d548250772364dcbaafed153a6b
Petro Palagin <petepalagin@gmail.com>
2025-11-25 17:10:01 +0200

    hw_5 is completed

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 ### Homework #1
+
+Description hw_5
--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+let usersAge = parseFloat(prompt("Please, enter your age:"));
+
+if (isNaN(usersAge)) {
+    alert("Age is not defined.");
+} else if (usersAge < 18) {
+    let adultPermission = confirm("Your age is under 18. Is there any adult, who allows you to watch it?");
+    if (adultPermission === true) {
+        alert("Access is allowed by an adult.");
+    } else {
+        alert("Access is not allowed.");
+    }
+} else if (usersAge >= 18) {
+    alert("Access is allowed, Enjoy your time here!");
+}
