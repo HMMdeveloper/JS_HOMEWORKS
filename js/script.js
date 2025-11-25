bbd8b8c4d1f8f65e052026b515ddbef6bf45256c
Petro Palagin <petepalagin@gmail.com>
2025-11-25 18:31:40 +0200

    hw_6 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const usersName = prompt("Please enter your name:");
+const usersAge = prompt("Please enter your age:");
+const usersCity = prompt("Please enter city where you live:");
+let doYouLikeJs = prompt("Do you like JavaScript(Yes/No)?");
+let usersAttitude;
+
+if (doYouLikeJs === "Yes" || doYouLikeJs === "yes" || doYouLikeJs === "YES") {
+    doYouLikeJs = !!doYouLikeJs;
+    usersAttitude = "You like JS!";
+} else {
+    doYouLikeJs = "";
+    doYouLikeJs = !!doYouLikeJs;
+    usersAttitude = "You don't like JS";
+};
+
+
+
+alert(`Hi, ${usersName}! You're ${usersAge} and you're from ${usersCity}. Your attitude towards JavaScript: ${usersAttitude}`);
+
+console.log(doYouLikeJs, typeof doYouLikeJs);
