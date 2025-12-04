0c50d5fe3994bd3850fc2b8b145adce2ecc1865e
Petro Palagin <petepalagin@gmail.com>
2025-12-04 18:50:16 +0200

    hw_9 is fixed

--- js/discountInAShop.js
+++ js/discountInAShop.js
@@ -1,13 +1,13 @@
-// 'use strict';
+'use strict';
 
-// const hasPromoCode = (confirm("Do you have a promocode?"));
-// const cartTotal = Number(prompt("Please enter the total in your wallet:"));
-// const isBlackFriday = (confirm("Is today a black friday?"));
-// const isDiscountApplied = (cartTotal >= 100 && hasPromoCode === true) || isBlackFriday === true;
-// const noDiscount = !isDiscountApplied;
+const hasPromoCode = (confirm("Do you have a promocode?"));
+const cartTotal = Number(prompt("Please enter the total in your wallet:"));
+const isBlackFriday = (confirm("Is today a black friday?"));
+const isDiscountApplied = (cartTotal >= 100 && hasPromoCode === true) || isBlackFriday === true;
+const noDiscount = !isDiscountApplied;
 
-// if (isDiscountApplied === true) {
-//     console.log("Discount is applied.");
-// } else {
-//     console.log("Discount is not applied.");
-// }
+if (isDiscountApplied === true) {
+    console.log("Discount is applied.");
+} else {
+    console.log("Discount is not applied.");
+}
