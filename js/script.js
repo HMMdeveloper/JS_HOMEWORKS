0393926e1b5f44bd70a43b5628ed7fcd8a63c7ef
Petro Palagin <petepalagin@gmail.com>
2025-12-05 18:07:15 +0200

    hw_13 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (const value of arr) {
+    sum += value;
+};
+
+console.log(sum);
+
+let squareSum = 0;
+
+for (const value of arr) {
+    squareSum += Math.pow(value, 2);
+};
+
+console.log(squareSum);
