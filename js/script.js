141861296c4ea62b39fdb2523bbe4edf8238b013
Petro Palagin <petepalagin@gmail.com>
2025-12-09 19:46:06 +0200

    hw_16 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const initialArray = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index !== -1) {
+        array.splice(index, 1);
+    } else {
+        console.log("Not valid number.");
+    }
+}
+
+removeElement(initialArray, 3);
+console.log(initialArray);
