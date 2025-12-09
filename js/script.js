fc67e3a6b8c35806b40c3595a2d04617e0e9a2b2
Petro Palagin <petepalagin@gmail.com>
2025-12-09 15:09:10 +0200

    hw_15 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function padString(string, stringLength, character, whichSide) {
+    let firstStep = "";
+    if (arguments.length < 4) {
+        return console.log("Not enough arguments.")
+    } else {
+        if (whichSide === false) {
+            firstStep = character += string;
+        } else {
+            firstStep = string += character;
+        }
+        return console.log(firstStep.substring(0, stringLength));
+    }
+};
+
+padString("Hello", 7, "*", true);
+
+// В начале условия просили вернуть все аргументы поэтому не понял
+// как можно венуть без true как в конце в примере
