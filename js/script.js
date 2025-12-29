7a7878c45b7aa3ba7b0d2754c50a389108099184
Petro Palagin <petepalagin@gmail.com>
2025-12-29 23:19:26 +0200

    hw_23 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const newArr = [1, 2, 3, [4, 5, [6]]];
+
+function myFlat(arr) {
+    if (arguments.length !== 1) {
+        throw new Error('Function accepts only 1 argument, too much arguments provided');
+    }
+
+    if (!Array.isArray(arr)) {
+        throw new TypeError('Argument must be an array');
+    }
+
+    const result = [];
+
+    function flatFunction(innerArr) {
+        for (const item of innerArr) {
+            if (Array.isArray(item)) {
+                flatFunction(item);
+            } else {
+                result.push(item);
+            }
+        }
+    }
+    flatFunction(arr);
+
+    return result;
+};
+
+console.log(myFlat(newArr));
