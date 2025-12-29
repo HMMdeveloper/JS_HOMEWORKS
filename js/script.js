9ef7ca95f74e47d23cfbdcffa13f2894bcc6456c
Petro Palagin <petepalagin@gmail.com>
2025-12-29 19:09:12 +0200

    hw_22 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let ulObj = document.getElementsByTagName("li");
+const ulArr = Array.from(ulObj);
+
+for (const value of ulArr) {
+    console.log(value);
+};
+
+// ----------------------------
+
+console.log(ulArr.length);
+
+// ----------------------------
+
+function textArr() {
+    const newArr = [];
+    for (const value of ulArr) {
+        newArr.push(value.textContent);
+    };
+    console.log(newArr);
+}
+
+textArr();
