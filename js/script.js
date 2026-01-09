59f4228014ae56c6a413fa31a094bd597ddad689
Petro Palagin <petepalagin@gmail.com>
2026-01-09 17:53:44 +0200

    hw_24 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const cardsContainer = document.querySelector('.cardsClass');
+
+cardsContainer.addEventListener('click', function (event) {
+    const button = event.target.closest('[data-read-more-btn]');
+    if (!button) return;
+
+    const card = button.closest('.card');
+
+    card.classList.toggle('expanded');
+
+    if (card.classList.contains('expanded')) {
+        button.textContent = 'Hide';
+    } else {
+        button.textContent = 'Read more';
+    }
+});
