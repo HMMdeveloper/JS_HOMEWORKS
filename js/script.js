8f1c03b53e7d3901a4d131d080fe76a27babc0e5
Petro Palagin <petepalagin@gmail.com>
2026-01-13 18:06:25 +0200

    hw_25 is completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const input = document.getElementById('input');
+const addBtn = document.getElementById('addBtn');
+const list = document.querySelector('.list');
+
+/* ADD */
+addBtn.addEventListener('click', () => {
+    const text = input.value.trim();
+    if (!text) return;
+
+    const li = document.createElement('li');
+    li.textContent = text + ' ';
+
+    const btn = document.createElement('button');
+    btn.textContent = '✖';
+    btn.className = 'remove';
+
+    li.append(btn);
+    list.append(li);
+
+    input.value = '';
+});
+
+list.addEventListener('click', (event) => {
+    const target = event.target;
+
+    if (target.classList.contains('remove')) {
+        target.closest('li').remove();
+        return;
+    }
+
+    const li = target.closest('li');
+    if (li && list.contains(li)) {
+        li.classList.toggle('selected');
+    }
+});
