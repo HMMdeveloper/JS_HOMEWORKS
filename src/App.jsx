2bad5b13331bd4e3f03c5138fce746f94b3dae21
Petro Palagin <petepalagin@gmail.com>
2026-03-12 20:27:17 +0200

    hw_37 is complited

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card.jsx'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Card />
+                <Card />
+            </div>
+        )
+    }
+}
+
+export default App;
